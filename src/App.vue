1c049f7b85208b787ba6d5eb4a602e108b715538
Harry Tran <nhattq.coding@gmail.com>
2020-06-01 15:22:34 +0700

    feat: use model and  vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <date-picker
+      v-model="date"
+      format="DD-MM-YYYY"
+      :options="{ firstDay: 2 }"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DatePicker from './components/common/DatePicker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    DatePicker,
+  },
+  data: () => ({
+    date: '10-10-2020',
+  }),
 }
 </script>
 
---
+++ src/components/common/DatePicker.vue
@@ -0,0 +1,30 @@
+<template>
+  <input :value="datePicked" type="text" ref="picked" />
+</template>
+
+<script>
+import Pickaday from 'pikaday'
+export default {
+  props: {
+    datePicked: { required: true },
+    format: { default: 'DD-MM-YYYY' },
+    options: { default: {} },
+  },
+  model: {
+    prop: 'datePicked',
+    event: 'picked',
+  },
+  mounted() {
+    const picker = new Pickaday({
+      field: this.$refs.picked,
+      format: this.format,
+      onSelect: () => {
+        this.$emit('picked', picker.toString())
+      },
+      ...this.options,
+    })
+  },
+}
+</script>
+
+<style></style>

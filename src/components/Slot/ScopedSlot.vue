158eaf6529f11e9156fb8e895f21d9c160c79420
Harry Tran <nhattq.coding@gmail.com>
2020-06-24 16:27:11 +0700

    feat: use slot shorthand & named slot

--- src/components/Slot/ScopedSlot.vue
+++ src/components/Slot/ScopedSlot.vue
@@ -1,17 +1,19 @@
 <template>
   <span class="text-xl text-red-600">
-    <slot :user="user">{{ user.lastName }}</slot>
+    <slot name="name" :user="user"></slot>
+    <slot name="age" :user="user"></slot>
   </span>
 </template>
 
 <script>
 export default {
-  name: 'CurrentUser',
+  name: "CurrentUser",
   data: () => ({
     user: {
-      firstName: 'Fuck',
-      lastName: 'Boy',
-    },
-  }),
-}
+      firstName: "Fuck",
+      lastName: "Boy",
+      age: 20
+    }
+  })
+};
 </script>
--- src/components/Slot/TestSlot.vue
+++ src/components/Slot/TestSlot.vue
@@ -2,20 +2,20 @@
   <fragment>
     <h1 class="text-2xl">Try to take one value of User</h1>
     <current-user>
-      <template v-slot:xxx="userProp">
-        {{ userProp.user.lastName }}
-      </template>
+      <template #name="{ user: { lastName } }">{{ lastName }}</template>
+      <br />
+      <template #age="{ user }">{{ user.age }}</template>
     </current-user>
   </fragment>
 </template>
 
 <script>
-import CurrentUser from './ScopedSlot'
+import CurrentUser from "./ScopedSlot";
 export default {
   components: {
-    CurrentUser,
-  },
-}
+    CurrentUser
+  }
+};
 </script>
 
 <style></style>
